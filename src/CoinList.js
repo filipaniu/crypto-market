01c7f6f128934ad9ab06556c543be872ac7e019b
jbeynar <jakub.bejnarowicz@gmail.com>
2024-03-21 18:28:28 +0100

    Added unionBy to onmessage in WS Coin List

--- src/CoinList.js
+++ src/CoinList.js
@@ -17,42 +17,22 @@ function CoinList() {
     const [wsData, setWsData] = useState([]);
     const navigate = useNavigate();
 
+    let previousWsData;
+
     useEffect(() => {
-        // !ticker@arr
         const ws = new WebSocket("wss://stream.binance.com:9443/ws/!ticker@arr");
         ws.onopen = () => {
             console.log("Socket has been opened")
         };
         ws.onmessage = (msg) => {
             const data = JSON.parse(msg.data);
-            const sortedData = _.sortBy(data,['s']);
-            setWsData(sortedData)
-            // TODO problem z tym ze binance przesyla nie wszystkie valuty
+            const union = _.unionBy(data, previousWsData, 's');
+            const sortedData = _.sortBy(union, ['s']);
+            setWsData(sortedData);
+            previousWsData = sortedData;
         };
     }, []);
-    // "e": "24hrTicker",  // Event type
-    //     "E": 1672515782136,     // Event time
-    //     "s": "BNBBTC",      // Symbol
-    //     "p": "0.0015",      // Price change
-    //     "P": "250.00",      // Price change percent
-    //     "w": "0.0018",      // Weighted average price
-    //     "x": "0.0009",      // First trade(F)-1 price (first trade before the 24hr rolling window)
-    //     "c": "0.0025",      // Last price
-    //     "Q": "10",          // Last quantity
-    //     "b": "0.0024",      // Best bid price
-    //     "B": "10",          // Best bid quantity
-    //     "a": "0.0026",      // Best ask price
-    //     "A": "100",         // Best ask quantity
-    //     "o": "0.0010",      // Open price
-    //     "h": "0.0025",      // High price
-    //     "l": "0.0010",      // Low price
-    //     "v": "10000",       // Total traded base asset volume
-    //     "q": "18",          // Total traded quote asset volume
-    //     "O": 0,             // Statistics open time
-    //     "C": 86400000,      // Statistics close time
-    //     "F": 0,             // First trade ID
-    //     "L": 18150,         // Last trade Id
-    //     "n": 18151          // Total number of trades
+
     if (wsData.length > 0) {
         const rows = wsData.map((x, i) => {
             return <TableRow key={i} onClick={() => navigate("/coin?symbol=" + x.s)} className="coinRow">
@@ -69,24 +49,26 @@ function CoinList() {
 
         })
 
-        return <TableContainer>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell sx={{width: "0.5vw"}}></TableCell>
-                        <TableCell>Icons</TableCell>
-                        <TableCell>Symbol</TableCell>
-                        <TableCell>Price USD</TableCell>
-                        <TableCell>Change 24h %</TableCell>
-                        <TableCell>Volume USD 24h</TableCell>
-                        {/*<TableCell>Market Cap</TableCell>*/}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows}
-                </TableBody>
-            </Table>
-        </TableContainer>;
+        return <div>
+            <h2>Coin list ({wsData.length})</h2>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{width: "0.5vw"}}></TableCell>
+                            <TableCell>Icons</TableCell>
+                            <TableCell>Symbol</TableCell>
+                            <TableCell>Price USD</TableCell>
+                            <TableCell>Change 24h %</TableCell>
+                            <TableCell>Volume USD 24h</TableCell>
+                            {/*<TableCell>Market Cap</TableCell>*/}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows}
+                    </TableBody>
+                </Table>
+            </TableContainer></div>;
     } else {
         return "No data";
     }
